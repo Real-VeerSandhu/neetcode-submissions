50a9633b723f5b18df315a8f094bdbc9b2359f71
Veer Sandhu <56776466+Real-VeerSandhu@users.noreply.github.com>
2026-08-23 13:59:33 -0400

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        # first find correct ROW!!
+
+        """
+        0 1 2 3 4
+        0 1 2 3 4
+        
+        
+        0 1 2 3 4
+        5 6 7 8 9
+
+
+        """
+
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+
+        l = 0
+        r = ROWS * COLS - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            row = m // COLS
+            col = m % COLS
+
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                l = m + 1
+            if matrix[row][col] > target:
+                r = m - 1
+        return False
+
+
