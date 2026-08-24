fba2b0bf3a188d3e41b99a57f44d5ecd8b5c9330
Veer Sandhu <56776466+Real-VeerSandhu@users.noreply.github.com>
2026-08-24 11:16:30 -0400

    Add: sliding-window-maximum - submission-5

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = deque() # monotinic decreasing Q from left->right
+        # left/front = maximum
+
+        l = 0
+        r = 0
+
+        while r < len(nums):
+
+
+            # new element nums[r] makes every element SMALLER THAN IT obselete
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+            
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+            print()
+        
+        return output
+
