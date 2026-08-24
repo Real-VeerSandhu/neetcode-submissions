eb4075448c2600f3f267fa0ae2afb54f64844c03
Veer Sandhu <56776466+Real-VeerSandhu@users.noreply.github.com>
2026-08-23 22:18:28 -0400

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """
+            deque is monotonic decreasing, left is big, right is small
+             left is always the max, and will be added to res
+             we will pop from right whenver adding soemthing that v
+             violated the monotonci decreasing property
+        """
+    
+        heap = []
+        output = []
+
+        for i, num in enumerate(nums):
+            heapq.heappush(heap, (-num, i))
+            if i <= k - 2:
+                continue
+
+            while heap[0][1] <= i - k:
+                heapq.heappop(heap)
+            output.append(-heap[0][0])
+
+        return output
