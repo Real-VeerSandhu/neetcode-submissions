e17e3c751eec7ed911e8d004ac706d6c8024891e
Veer Sandhu <56776466+Real-VeerSandhu@users.noreply.github.com>
2026-09-12 02:22:21 -0400

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # 2 facts
+        # 1st fact: first value in preorder traversal = ROOT of the sub tree ur on
+        # 2nd fact: every value to the left of the root, will be in left subtree
+        # every value to the right of root will be in right subtree
+
+        # if not preorder or not inorder:
+        #     return None
+        
+        # root = TreeNode(preorder[0])
+
+        # mid = inorder.index(preorder[0])
+
+        # root.left = self.buildTree(preorder[1:mid + 1], inorder[: mid])
+        # root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1 :])
+
+        # return root
+
+        indices = {}
+        for i, val in enumerate(inorder):
+            indices[val] = i
+
+        self.pre_idx = 0
+
+        def dfs(l, r):
+            if l > r:
+                return None
+            
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+
+            root = TreeNode(val=root_val)
+
+            mid_idx = indices[root_val]
+            root.left = dfs(l, mid_idx - 1)
+            root.right = dfs(mid_idx + 1, r)
+            return root
+        
+        return dfs(0, len(inorder) - 1)
